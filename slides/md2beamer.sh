53aad1858248da37a163acb0da3e9e7af6af6a2e
John McCrae <john@mccr.ae>
2014-05-21 15:55:44 +0200

    Use https urls

--- slides/md2beamer.sh
+++ slides/md2beamer.sh
@@ -7,11 +7,8 @@ EXTENSION=${FILE##*.}
 if [[ $EXTENSION == "md" ]]
 then
     pandoc -t slidy -s $FILE -o ${FILE/%.md/.html} -H $DIR/header --mathjax
-    if [[ -z $1 ]]
-    then
-        perl -pi -e 's/http:\/\/www.w3.org\/Talks/\/\/www.w3.org\/Talks/' ${FILE/%.md/.html}
-        perl -pi -e 's/http:\/\/cdn/\/\/cdn/' ${FILE/%.md/.html}
-    fi
+    perl -pi -e 's/http:\/\/www.w3.org\/Talks/https:\/\/www.w3.org\/Talks/' ${FILE/%.md/.html}
+    perl -pi -e 's/http:\/\/cdn.mathjax.org/https:\/\/c328740.ssl.cf1.rackcdn.com/' ${FILE/%.md/.html}
 else
     echo "Please specify a Markdown file with .md extension"
 fi
